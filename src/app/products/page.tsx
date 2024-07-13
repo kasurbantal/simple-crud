1d701ca2573184540f914cfd60dd9ac9576118f4
kasurbantal <kasurbantal880@gmail.com>
2024-07-13 19:46:08 +0700

    fetching data from db.json

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,40 @@
-export default function ProductList () {
+type Product = {
+    id: number;
+    title: string;
+    price: number;
+}
+
+async function getProducts() {
+    const res = await fetch('http://localhost:5000/product', {
+        cache: "no-store"
+        });
+    return res.json();
+}
+
+export default async function ProductList () {
+    const products : Product[] = await getProducts();
     return (
-        <div>Product</div>
+        <div className="py-10 px-10">
+            <table className="table w-full">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Product Name</th>
+                        <th>Price</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                    <tbody>
+                    {products.map((product, index) => (
+                        <tr key={product.id}>
+                            <td>{index + 1}</td>
+                            <td>{product.title}</td>
+                            <td>{product.price}</td>
+                            <td></td>
+                        </tr>
+                    ))}
+                    </tbody>
+            </table>
+        </div>
     )
   }
